1047ae68621e2116806088bd0ff6a6b8b56d0d4a
Naksh Rathore <naksh.rathore786@gmail.com>
2026-02-16 19:12:08 -0600

    Update editor.lua with new formatting

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,5 @@
 return {
+    -- Existing plugins
     {
         "lewis6991/gitsigns.nvim",
         opts = {},
@@ -20,46 +21,51 @@ return {
             }
         end
     },
+
+    -- VS Code-like bufferline (tabs)
     {
         "akinsho/bufferline.nvim",
         version = "*",
         dependencies = "nvim-tree/nvim-web-devicons",
         opts = {
             options = {
-                mode = "buffers",
-                separator_style = "slant", -- try "thin" or "padded_slant"
-                diagnostics = "nvim_lsp",
-                always_show_bufferline = true,
-                show_buffer_close_icons = false,
+                numbers = "ordinal",
+                indicator = { style = "underline" },
+                show_buffer_close_icons = true,
                 show_close_icon = false,
+                tab_size = 18,
+                diagnostics = "nvim_lsp",
             },
         },
     },
+
+    -- VS Code-like file explorer
     {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    opts = {
-        sort_by = "name",
-        view = {
-            width = 30,
-            side = "left",
-            adaptive_size = true,
-        },
-        renderer = {
-            group_empty = true,
-        },
-        filters = {
-            dotfiles = false,
-        },
-        git = {
-            enable = true,
-            ignore = false,
-        },
+        "nvim-tree/nvim-tree.lua",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("nvim-tree").setup({
+                disable_netrw = true,
+                hijack_netrw = true,
+                update_cwd = true,
+                view = { width = 30, side = "left", float = { enable = false } },
+                renderer = {
+                    icons = {
+                        show = {
+                            file = true,
+                            folder = true,
+                            folder_arrow = true,
+                            git = true,
+                        },
+                    },
+                },
+                actions = {
+                    open_file = { quit_on_open = false, resize_window = true },
+                },
+            })
+            vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+        end
     },
-    config = function()
-        require("nvim-tree").setup({})
-    end
-}
 
 }
 
